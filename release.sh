7d24c14f4d584af83172d65c85b64d3170ce7460
carlos <karl.catewicz@fit.fraunhofer.de>
2015-05-19 17:53:59 +0200

    {} don't like quotations

--- release.sh
+++ release.sh
@@ -35,41 +35,49 @@ do
     # Copy configuration
     mkdir -p "$d/conf/devices"
     mkdir -p "$d/conf/services"
-    cp -R "${DIR}/conf/*.json" "$d/conf/"
-    cp -R "${DIR}/conf/devices/*.json" "$d/conf/devices/"
-    cp -R "${DIR}/conf/services/*.json" "$d/conf/services/"
+    echo "fuuu1"
+
+    cp -Rv ${DIR}/conf/*.json "$d/conf/"
+    echo "fuuu2"
+
+    cp -Rv ${DIR}/conf/devices/*.json "$d/conf/devices/"
+    echo "fuuu2"
+
+    cp -Rv ${DIR}/conf/services/*.json "$d/conf/services/"
+    echo "fuuu2"
+
 
     # Copy examples of agents
     mkdir "$d/agent-examples"
-    cp -R "${DIR}/agent-examples/*" "$d/agent-examples"
+    cp -Rv ${DIR}/agent-examples/* "$d/agent-examples"
 
     # Copy static
     mkdir -p "$d/static/"
-    cp -R "${DIR}/static/ctx" "$d/static/"
+    cp -Rv ${DIR}/static/ctx "$d/static/"
 
     # Copy dashboard
     mkdir -p "$d/static/dashboard/css"
-    cp -R "${DIR}/static/dashboard/css/freeboard.min.css" "$d/static/dashboard/css/"
+    cp -Rv ${DIR}/static/dashboard/css/freeboard.min.css "$d/static/dashboard/css/"
     mkdir -p "$d/static/dashboard/img"
-    cp -R "${DIR}/static/dashboard/img/dropdown-arrow.png" "$d/static/dashboard/img/"
-    cp -R "${DIR}/static/dashboard/img/glyphicons-halflings-white.png" "$d/static/dashboard/img/"
-    cp -R "${DIR}/static/dashboard/img/glyphicons-halflings.png" "$d/static/dashboard/img/"
-    cp -R "${DIR}/static/dashboard/index.html" "$d/static/dashboard/"
+    cp -Rv ${DIR}/static/dashboard/img/dropdown-arrow.png "$d/static/dashboard/img/"
+    cp -Rv ${DIR}/static/dashboard/img/glyphicons-halflings-white.png "$d/static/dashboard/img/"
+    cp -Rv ${DIR}/static/dashboard/img/glyphicons-halflings.png "$d/static/dashboard/img/"
+    cp -Rv ${DIR}/static/dashboard/index.html "$d/static/dashboard/"
     mkdir -p "$d/static/dashboard/js"
-    cp -R "${DIR}/static/dashboard/js/freeboard+plugins.min.js" "$d/static/dashboard/js/"
-    cp -R "${DIR}/static/dashboard/js/freeboard+plugins.min.js.map" "$d/static/dashboard/js/"
-    cp -R "${DIR}/static/dashboard/js/freeboard.min.js" "$d/static/dashboard/js/"
-    cp -R "${DIR}/static/dashboard/js/freeboard.min.js.map" "$d/static/dashboard/js/"
-    cp -R "${DIR}/static/dashboard/js/freeboard.plugins.min.js" "$d/static/dashboard/js/"
-    cp -R "${DIR}/static/dashboard/js/freeboard.plugins.min.js.map" "$d/static/dashboard/js/"
-    cp -R "${DIR}/static/dashboard/js/freeboard.thirdparty.min.js" "$d/static/dashboard/js/"
+    cp -Rv ${DIR}/static/dashboard/js/freeboard+plugins.min.js "$d/static/dashboard/js/"
+    cp -Rv ${DIR}/static/dashboard/js/freeboard+plugins.min.js.map "$d/static/dashboard/js/"
+    cp -Rv ${DIR}/static/dashboard/js/freeboard.min.js "$d/static/dashboard/js/"
+    cp -Rv ${DIR}/static/dashboard/js/freeboard.min.js.map "$d/static/dashboard/js/"
+    cp -Rv ${DIR}/static/dashboard/js/freeboard.plugins.min.js "$d/static/dashboard/js/"
+    cp -Rv ${DIR}/static/dashboard/js/freeboard.plugins.min.js.map "$d/static/dashboard/js/"
+    cp -Rv ${DIR}/static/dashboard/js/freeboard.thirdparty.min.js "$d/static/dashboard/js/"
     mkdir -p "$d/static/dashboard/plugins/freeboard"
     mkdir -p "$d/static/dashboard/plugins/thirdparty"
-    cp -R "${DIR}/static/dashboard/plugins/freeboard/freeboard.datasources.js" "$d/static/dashboard/plugins/freeboard/"
-    cp -R "${DIR}/static/dashboard/plugins/freeboard/freeboard.widgets.js" "$d/static/dashboard/plugins/freeboard/"
-    cp -R "${DIR}/static/dashboard/plugins/thirdparty/jquery.sparkline.min.js" "$d/static/dashboard/plugins/thirdparty/"
-    cp -R "${DIR}/static/dashboard/plugins/thirdparty/justgage.1.0.1.js" "$d/static/dashboard/plugins/thirdparty/"
-    cp -R "${DIR}/static/dashboard/plugins/thirdparty/raphael.2.1.0.min.js" "$d/static/dashboard/plugins/thirdparty/"
+    cp -Rv ${DIR}/static/dashboard/plugins/freeboard/freeboard.datasources.js "$d/static/dashboard/plugins/freeboard/"
+    cp -Rv ${DIR}/static/dashboard/plugins/freeboard/freeboard.widgets.js "$d/static/dashboard/plugins/freeboard/"
+    cp -Rv ${DIR}/static/dashboard/plugins/thirdparty/jquery.sparkline.min.js "$d/static/dashboard/plugins/thirdparty/"
+    cp -Rv ${DIR}/static/dashboard/plugins/thirdparty/justgage.1.0.1.js "$d/static/dashboard/plugins/thirdparty/"
+    cp -Rv ${DIR}/static/dashboard/plugins/thirdparty/raphael.2.1.0.min.js "$d/static/dashboard/plugins/thirdparty/"
 
     # Copy docs
     #cp -R ${DIR}/docs $d/
