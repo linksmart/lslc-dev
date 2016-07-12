c336f7775acc14e2202b3bfde0c9290ce76bb3f0
Alexandr Krylovskiy <alexandr.krylovskiy@gmail.com>
2016-07-12 13:16:24 +0200

    script for adding copyright headers

---
+++ add-copyright-header.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#############################################################
+# Simple script to add copyright header to the source files
+############################################################
+
+COPYRIGHT="// Copyright 2016 Fraunhofer Insitute for Applied Information Technology FIT"
+
+echo $COPYRIGHT > copyright-header
+echo "" >> copyright-header
+
+# go code
+for i in `find src/ -iname "*.go"`
+do
+  if ! grep -q Copyright ${i}
+  then
+	echo "Adding header to ${i}..."
+    	cat copyright-header ${i} > ${i}.new && mv ${i}.new ${i}
+  fi
+done
+
+rm copyright-header
